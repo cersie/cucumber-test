6e9bff455881d9eadc8830fec607f31a32893ebb
Neetu Meena <neetumeena59@gmail.com>
2017-09-24 21:34:42 +0530

    adding api test for list users

--- src/api/APITest.java
+++ src/api/APITest.java
@@ -1,10 +1,25 @@
-import static org.junit.Assert.assertEquals;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
+import org.junit.Before;
 
 public class APITest {
+
+    @Before
+    public void setupBase() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @Test
+    public void verifyEndPointIsWorking() {
+        given().when().get("/api/users").then().statusCode(200);
+    }
+
     @Test
     public void verifyListUsers() {
-      assertEquals(1, 1);
+        given().when().get("/api/users").then().body("page", equalTo(1));
     }
 }
   
