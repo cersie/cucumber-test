723b832306baffa2a377f0ff850a3e2486b9b3e6
Neetu Meena <neetumeena59@gmail.com>
2017-09-24 21:07:10 +0530

    adding API test skeleton

---
+++ src/api/APITest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class APITest {
+    @Test
+    public void verifyListUsers() {
+      assertEquals(1, 1);
+    }
+}
+  
